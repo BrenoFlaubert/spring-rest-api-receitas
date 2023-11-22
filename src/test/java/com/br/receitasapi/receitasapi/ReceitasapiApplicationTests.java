b115c9f34a0dcee6fad306967d14730bd0605130
Breno Flaubert <brenoflaubert@gmail.com>
2023-11-22 09:30:41 -0300

    feat: first commit

---
+++ src/test/java/com/br/receitasapi/receitasapi/ReceitasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.receitasapi.receitasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
